354a9b53ff6235725fb3349e2b9a4eb0e45b3e8d
Mambalam <laliths171@gmail.com>
2020-11-15 08:14:53 -0500

    Adding day 3 solved files

---
+++ 01-Lesson-Plans/03-Python/3/Activities/03-Stu_HobbyBook-Dictionaries/Solved/HobbyBook_Solved.py
@@ -0,0 +1,13 @@
+# Dictionary full of info
+my_info = {"name": "Rex",
+           "occupation": "dog",
+           "age": 21,
+           "hobbies": ["barking", "eating", "sleeping", "loving my owner"],
+           "wake-up": {"Mon": 5, "Friday": 5, "Saturday": 10, "Sunday": 9}}
+
+# Print out results are stored in the dictionary
+#print(f'Hello I am {my_info["name"]} and I am a {my_info["occupation"]}')
+print(f'My name is {my_info["name"]}, I have {my_info(len["hobbies"])} ')
+#and I woke up at {my_info["wakeUp"][0]}')
+#print(f'I have {len(my_info["hobbies"])} hobbies!')
+#print(f'On the weekend I get up at {my_info["wake-up"]["Saturday"]}')
